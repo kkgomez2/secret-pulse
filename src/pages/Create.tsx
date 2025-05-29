11cd63f2f3af146306aa623134b65ed8d8d2f2ba
kkgomez2 <kkristoffergomez@gmail.com>
2025-05-28 21:54:26 -0700

    Basic question form

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
+
+type Question = {
+  id: number;
+  question: string;
+};
+
 const Create = () => {
+  const [questionForm, setQuestionForm] = useState<Array<Question>>([]);
+
   const addQuestion = () => {
-    console.log("Question Added");
+    const newForm = questionForm;
+    newForm.push({
+      id: questionForm.length + 1,
+      question: "What?",
+    });
+    setQuestionForm([...newForm]);
   };
   return (
     <>
@@ -14,6 +28,17 @@ const Create = () => {
         >
           Add question
         </button>
+
+        <section>
+          {questionForm.map(({ id, question }, i) => {
+            return (
+              <div key={i}>
+                <h2>Question {id}</h2>
+                <p>{question}</p>
+              </div>
+            );
+          })}
+        </section>
       </div>
     </>
   );
